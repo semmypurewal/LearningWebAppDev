f25682aa9294b814880b483dac150cb1c84074a4
Semmy Purewal <semmypurewal@gmail.com>
2014-03-16 15:39:20 -0700

    Add tweet counter (forgot in last commit)

---
+++ Chapter6/app/Twitter/tweet_counter.js
@@ -0,0 +1,29 @@
+var ntwitter = require('ntwitter'),
+    credentials = require('./credentials.json'),
+    twitter,
+    counts = {};
+
+// set up our twitter objects
+twitter = ntwitter(credentials);
+
+// initialize our counters
+counts.awesome = 0;
+
+twitter.stream(
+    'statuses/filter',
+    { track: ["awesome"] },
+    function(stream) {
+        stream.on('data', function(tweet) {
+            if (tweet.text.indexOf("awesome") >= 0) {
+                // increment the awesome counter
+                counts.awesome = counts.awesome + 1;
+            }
+        });
+    }
+);
+
+setInterval(function () {
+    console.log("awesome: " + counts.awesome);
+}, 3000);
+
+module.exports = counts;
